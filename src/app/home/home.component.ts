f30cc4ab71afe2a2267c56778206b0eb2d28c0e8
valerioprds <v.paradiso89@gmail.com>
2023-12-06 12:42:48 +0100

    sidenav completed no errors now

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
+  constructor(private observer: BreakpointObserver, private cdRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+        if (res.matches) {
+            this.sidenav.mode = 'over';
+            this.sidenav.close();
+        } else {
+            this.sidenav.mode = 'side';
+            this.sidenav.open();
+        }
+        this.cdRef.detectChanges();
+    });
+}
 }
