ea8fe4e6ff579cb165b499e0d68e23dab8109e82
valerioprds <v.paradiso89@gmail.com>
2024-02-07 11:38:44 +0100

    added some fake pics

--- src/app/photogallery/weddings/weddings.component.ts
+++ src/app/photogallery/weddings/weddings.component.ts
@@ -6,19 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./weddings.component.scss'],
 })
 export class WeddingsComponent {
-  photos = [
-    {
-      url: '/assets/weddings-img/wedding2.jpg',
-      title: 'Photo 1',
-      description: 'Description of photo 2',
-    },
-    {
-      url: '/assets/weddings-img/wedding2.jpg',
-      title: 'Photo 1',
-      description: 'Description of photo 2',
-    },
-    // Add more photos here
-  ];
 
   constructor() {}
 
