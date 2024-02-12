7f9f2847d74333193c11a92e023308baa8249951
valerioprds <v.paradiso89@gmail.com>
2024-02-12 14:49:26 +0100

    modified gallery layout

--- src/app/photogallery/wedding1/wedding1.component.ts
+++ src/app/photogallery/wedding1/wedding1.component.ts
@@ -8,12 +8,18 @@ import { Component } from '@angular/core';
 export class Wedding1Component {
   images = [
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 1' },
-    { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 2' },
+    {
+      src: '/assets/weddings-img/wedding-cake.jpg',
+      alt: 'Image description 2',
+    },
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 1' },
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 2' },
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 1' },
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 2' },
-    { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 1' },
+    {
+      src: '/assets/weddings-img/wedding-cake.jpg',
+      alt: 'Image description 1',
+    },
     { src: '/assets/weddings-img/wedding1.webp', alt: 'Image description 2' },
     // Add more images as needed
   ];
