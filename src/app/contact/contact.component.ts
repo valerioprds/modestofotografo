b8ec4384804a6e71e5ad5faab871c93cc0148bf0
valerioprds <v.paradiso89@gmail.com>
2024-02-16 12:19:02 +0100

    contact form modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-contact',
@@ -9,4 +11,11 @@ export class ContactComponent {
   email: string = 'mimail@example.com';
   telefono: string = '+34 123 456 789';
   instagramUsername: string = 'tu_usuario';
+
+  constructor(public dialogRef: MatDialogRef<ContactComponent>) {}
+
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
 }
